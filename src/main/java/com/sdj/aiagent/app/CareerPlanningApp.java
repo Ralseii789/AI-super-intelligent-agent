f3e37dfb997ff7970731454fa2a934ad655121c7
Ralsei <1178803912@qq.com>
2026-05-13 18:57:52 +0800

    自定义Advisor与结构化输出

--- src/main/java/com/sdj/aiagent/app/CareerPlanningApp.java
+++ src/main/java/com/sdj/aiagent/app/CareerPlanningApp.java
@@ -1,11 +1,15 @@
 package com.sdj.aiagent.app;
 
+import com.sdj.aiagent.advisor.MyLoggerAdvisor;
+import com.sdj.aiagent.advisor.ReReadingAdvisor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
@@ -29,7 +33,6 @@ public class CareerPlanningApp {
             引导用户更深入地分享自己的处境、感受、过往经历、技能偏好或对未来的想象。
             当用户描述了一个职业问题后，你可以先共情，然后追问具体情境中的细节，
             逐步拼凑出用户的全貌。在获得足够信息后，再给出个性化、可行动的职业分析或转型路径建议。
-            你的语气要温暖、支持，不带评判，且永远给用户留出选择的空间。
             """;
 
     /**
@@ -39,7 +42,13 @@ public class CareerPlanningApp {
     public CareerPlanningApp(ChatClient.Builder builder){
         InMemoryChatMemory inMemoryChatMemory = new InMemoryChatMemory();
         this.chatClient = builder.
-                defaultAdvisors(new MessageChatMemoryAdvisor(inMemoryChatMemory)).
+                defaultAdvisors(
+                        new MessageChatMemoryAdvisor(inMemoryChatMemory),
+                        //自定义日志Advisor 可按需开启
+                        new MyLoggerAdvisor()
+                        //自定义推理增强Advisor 可按需开启
+                        //new ReReadingAdvisor()
+                ).
                 defaultSystem(SYSTEM_PROMPT).
                 build();
     }
@@ -60,5 +69,24 @@ public class CareerPlanningApp {
         String content = chatResponse.getResult().getOutput().getText();
         log.info("content: {}",content);
         return content;
+    }
+
+    record CareerReoprt(String title, List<String> suggestions){}
+    /**
+     * AI 职业规划报告（实战结构化输出）
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public CareerReoprt doChatWithReport(String message,String chatId){
+        CareerReoprt careerReoprt = chatClient.prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成职业规划结果，标题为{用户名}的职业规划报告，内容为建议列表")
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .entity(CareerReoprt.class);
+        log.info("careerReoprt: {}",careerReoprt);
+        return careerReoprt;
     }
 }
